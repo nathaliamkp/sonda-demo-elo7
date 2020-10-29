bee177f72916a6a4d18f7969fb4a5d274f31148e
Nathalia Pitombeira <nah.manso@gmail.com>
2020-10-29 12:23:51 -0300

    configuração inicial do projeto

---
+++ src/main/java/br/com/elo7/sondademo/SondaDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.elo7.sondademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SondaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elo7/sondademo/SondaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elo7.sondademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
